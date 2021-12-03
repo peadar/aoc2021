ae6f5123adda9c30926a6381da5119cb5e26c22c
Peter Edwards <peadar@arista.com>
2021-12-03 00:32:19 +0000

    harder

--- day2/sm.cpp
+++ day2/sm.cpp
@@ -14,40 +14,29 @@ main()
       switch (targets[1]) {
          case 0:
             switch (c = getc(stdin)) {
+               case -1:
+                  printf("%d\n", targets[8] * targets[7]);
+                  exit(0);
                case ' ':
                   targets[1]++;
-                  break;
-               case -1:
-                  targets[1]+=3;
                   break;
                default:
                   ++l;
                   break;
             }
             break;
-
          case 1:
-            switch (c = getc(stdin)) {
-               case '\n':
-                  targets[l] += targets[0];
-                  targets[l+1] += (targets[4] - targets[2] ) * targets[0];
-                  targets[1]++;
-                  break;
-               default:
-                  targets[0] = targets[0] * 10 + c - 48;
-                  break;
+            if ((c = getc(stdin)) > 32) {
+               targets[0] = targets[0] * 10 + c - 48;
+               break;
             }
-            break;
-
-         case 2:
+            targets[l] += targets[0];
+            targets[l+1] += (targets[4] - targets[2] ) * targets[0];
             l = 0;
             targets[0] = 0;
-            targets[1]-=2;
+            targets[1]--;
             break;
 
-         case 3:
-            printf("%d\n", targets[8] * targets[7]);
-            exit(0);
       }
    }
 }
