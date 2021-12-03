8c601a514f35d690208a3c124e4dbe689919358a
Peter Edwards <peadar@arista.com>
2021-12-03 14:50:47 +0000

    harder

--- day2/sm.cpp
+++ day2/sm.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int i[11];
+int i[11], *j=i+10;
 int main() {
-   while((i[9]=getc(stdin)) != -1) {
+   while((i[9]=getc(stdin)-48) != -49) {
       if(i[1]&1) {
-         if (i[9] > 32) {
-            i[0] = i[0] * 10 + i[9] - 48;
+         if (i[9] > 0) {
+            i[0] = i[0] * 10 + i[9];
             continue;
          }
-         i[i[10]] += i[0];
-         i[i[10]+1] += (i[4] - i[2] ) * i[0];
-         i[10] = i[0] = 0;
+         i[*j] += i[0];
+         i[*j+1] += (i[4] - i[2] ) * *i;
+         *j = *i = 0;
          i[1]++;
          continue;
       }
-      i[i[9] == ' ' ? 1 : 10]++;
+      i[i[9] == -16 ? 1 : 10]++;
    }
    printf("%d\n", i[8] * i[7]);
 }
