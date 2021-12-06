93a8b4f86b21d7c914b104201c3e0ee19685dbda
Peter Edwards <peadar@arista.com>
2021-12-06 09:01:47 +0000

    Day 6

---
+++ day6/parts.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <numeric>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+   unsigned long counts[9] = {};
+   while (!std::cin.eof()) {
+      int i;
+      char comma;
+      std::cin >> i >> comma;
+      counts[i]++;
+   }
+
+   int target_days = argc >= 2 ?  target_days = strtoul(argv[1], 0, 0) : 80;
+   int mod = 0;
+   for (int i = 1; i <= target_days; ++i) {
+      unsigned long spawncount = counts[mod];
+      mod = (mod + 1) % 9;
+      counts[(mod + 6) % 9] += spawncount;
+      counts[(mod + 8) % 9] = spawncount;
+   }
+   std::cout << std::accumulate(counts, counts + 9, 0UL) << std::endl;
+}
