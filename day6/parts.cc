38275fd314be9c5639bd5e10189fa508e67a7791
Peter Edwards <peadar@arista.com>
2021-12-06 20:02:37 +0000

    Obvious simplification, and do part 1 and 2 together in the same run

--- day6/parts.cc
+++ day6/parts.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include <numeric>
-int main(int argc, char *argv[]) {
+#include <vector>
+#include <iterator>
+int main() {
    unsigned long counts[9] = {};
    while (!std::cin.eof()) {
       unsigned long i;
@@ -8,11 +11,7 @@ int main(int argc, char *argv[]) {
       std::cin >> i >> comma;
       counts[i]++;
    }
-   int target_days = argc >= 2 ? strtoul(argv[1], 0, 0) : 80;
-   for (int i = 0; i < target_days; ++i) {
-      unsigned long spawncount = counts[i%9];
-      counts[(i + 7) % 9] += spawncount;
-      counts[(i + 9) % 9] = spawncount;
-   }
-   std::cout << std::accumulate(counts, counts + 9, 0UL) << std::endl;
+   for (size_t i = 0; i != 257; counts[(i + 7) % 9] += counts[i%9], ++i)
+      if (i == 80 || i == 256)
+         std::cout << "day " << i << ": " << std::accumulate(counts, counts + 9, 0UL) << std::endl;
 }
--- day6/parts.py
+++ day6/parts.py
@@ -2,8 +2,6 @@ counts = [0]*9
 for initial in ( int(split) for split in open(".input").read().split(",") ):
     counts[initial]+=1
 for i in range(0, 256):
-    spawning = counts[i%9]
-    counts[(i + 9) % 9] = spawning
-    counts[(i + 7) % 9] += spawning
+    counts[(i + 7) % 9] += counts[(i + 9) % 9]
     if i == 79 or i == 255:
         print("day %d: %d" % (i+1, sum(counts)))
